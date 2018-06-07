cc45c07d77d65f5f893131517a5b3d700cc82f14
ashwiniu <ashwiniu@qgold.com>
2018-06-07 15:10:48 +0530

    changes nav

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
 $('document').ready(function () {
     // Init ScrollMagic
-    var controller = new ScrollMagic.Controller();
+    // var controller = new ScrollMagic.Controller();
 
-    // build a scene
-    var ourScene = new ScrollMagic.Scene({
-            triggerElement: '#test'
-        })
-        .setClassToggle('#test', 'fade-in') // add class to project01
-        .addIndicators({
-            name: 'fade scene',
-            colorTrigger: 'black',
-            indent: 200,
-            colorStart: '#75C695'
-        }) // this requires a plugin
-        .addTo(controller);
+    // // build a scene
+    // var ourScene = new ScrollMagic.Scene({
+    //         triggerElement: '#test'
+    //     })
+    //     .setClassToggle('#test', 'fade-in') // add class to project01
+    //     .addIndicators({
+    //         name: 'fade scene',
+    //         colorTrigger: 'black',
+    //         indent: 200,
+    //         colorStart: '#75C695'
+    //     }) // this requires a plugin
+    //     .addTo(controller);
+
+
 });
